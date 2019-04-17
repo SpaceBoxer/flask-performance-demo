7471c3aba790ba7faed31edae82b3768c374c192
yangshuanglong <slyang@aliyun.com>
2019-04-17 12:12:39 +0800

    Add configuration

--- demo/__init__.py
+++ demo/__init__.py
@@ -7,6 +7,7 @@ def create_app():
 
     app = Flask(__name__)
     pc = PerfomanceCollector(app)
+    app.config['METRIC_DSN'] = 'https://metrics.spacebox.fun/v1/collector/c5b35bc078844a59a15dd506e08f3ae6'
 
     return app
 
