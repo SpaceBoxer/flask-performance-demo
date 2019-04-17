b8dcd8d2dcaeacded22020e5fa9f32439604cde1
yangshuanglong <slyang@aliyun.com>
2019-04-17 13:21:05 +0800

    fix bug

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask
-from flask_performance import PerfomanceCollector
-
+from flask_performance import PerformanceCollector
 
 
 def create_app():
 
     app = Flask(__name__)
-    pc = PerfomanceCollector(app)
     app.config['METRIC_DSN'] = 'https://metrics.spacebox.fun/v1/collector/c5b35bc078844a59a15dd506e08f3ae6'
-
+    pc = PerformanceCollector(app)
+    
     return app
 
 
 app = create_app()
 
+from .views import users
